f00c33f6077c484270d2e3e5706b8d36941ed520
Wojtek <wojtex1221@gmail.com>
2020-04-21 21:55:04 +0200

    init virtual environment with numpy

---
+++ lab6/zad1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
+print(np.random.normal())
