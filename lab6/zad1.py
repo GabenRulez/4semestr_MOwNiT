779fb932b399addfe78417eba6010794e4d337a7
Wojciech <wojtex1221@gmail.com>
2020-04-22 00:50:23 +0200

    Added .pdf file with tasks to do.

--- lab6/zad1.py
+++ lab6/zad1.py
@@ -1,7 +1,18 @@
 import numpy as np
+import scipy as sp
 
-print(np.random.normal()/10)
+print(np.random.normal()/100)
+
+np.random.normal()
+
 
 # jejku, ile to zajmuje
 
 ####
+
+# np.linalg.solve()
+
+# np.linalg.lstsq()
+
+# sp.linalg.lu()
+
