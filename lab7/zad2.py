9803add2bdde4201ab377926fd09856de6296183
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-05-11 01:33:53 +0200

    Zrezygnowałem z używania własnej, wolniejszej i bardziej zawodnej funkcji. Ukończenie programu.

--- lab7/zad2.py
+++ lab7/zad2.py
@@ -14,84 +14,7 @@
 import numpy as np
 import scipy.linalg
 
-
-# Moja funkcja z lab6/zad2.py
-def rozklad_LUP(a):  # korzystam z algorytmu z Kincaida, str. 167
-    n = len(a)
-
-    u = np.copy(a)
-    u = u.astype(float)
-    p = tworz_macierz_kwadratowa_z_zerami(n)
-    dodaj_diagonale(p, 1)
-    l = tworz_macierz_kwadratowa_z_zerami(n)
-
-
-    for i in range(n):  # skalowanie
-        p[i] = p[i] / np.max(u[i])
-        u[i] = u[i] / np.max(u[i])
-
-    for i in range(n):
-
-        # częściowe poszukiwanie elementu wiodącego
-        max = u[i][i]
-        indeks = i
-
-        for j in range(i, n):
-            if u[j][i] > max:
-                max = u[j][i]
-                indeks = j
-
-        if indeks != i:
-            u[[indeks, i]] = u[[i, indeks]]
-            p[[indeks, i]] = p[[i, indeks]]
-            l[[indeks, i]] = l[[i, indeks]]
-
-
-        for k in range(i + 1, n):
-            czynnik = u[k][i] / u[i][i]
-            u[k] = u[k] - u[i] * czynnik
-            u[k][i] = 0
-            l[k][i] = czynnik
-
-    dodaj_diagonale(l, 1)
-    return p, l, u
-
-def tworz_macierz_kwadratowa(n, zakres_start=0, zakres_end=10, asint=False):
-    macierz = (zakres_end - zakres_start) * np.random.random_sample((n, n)) + zakres_start
-    if asint:
-        macierz = macierz.astype(int)
-    else:
-        macierz = macierz.astype(float)
-    return macierz
-
-def tworz_macierz_kwadratowa_z_zerami(n):
-    macierz = np.zeros((n, n))
-    macierz = macierz.astype(float)
-    return macierz
-
-def dodaj_diagonale(macierz, wartosc):
-    n = len(macierz)
-    for i in range(n):
-        macierz[i][i] = wartosc
-    return
-# Koniec funkcji z lab6/zad2.py
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from textFunctions import printLineSpacer, printTitle, printSpacer
 
 
 def odwrotnaMetodaPotegowa(matrix_A, iterations=1000, epsilon=0.000001, sigma=0):
@@ -99,9 +22,7 @@ def odwrotnaMetodaPotegowa(matrix_A, iterations=1000, epsilon=0.000001, sigma=0)
     vector_X = np.zeros(n)
     vector_X[0] = 1
 
-    p, l, u = rozklad_LUP(matrix_A)
-    LU = np.dot(l,u)
-    print(LU)
+    LU = scipy.linalg.lu_factor(matrix_A - sigma*np.identity(n))
 
     for i in range(iterations):
         new_vector_X = scipy.linalg.lu_solve(LU, vector_X)
@@ -119,9 +40,22 @@ A = np.array([
     [7,8,9]
 ])
 
+printTitle("np.linalg.eig")
+w,v = np.linalg.eig(A)
+print(w)
+printSpacer(1)
+print(v)
 
-odwrotnaMetodaPotegowa(A)
+printTitle("odwrotnaMetodaPotegowa", 1)
+print("Dla sigma = w[0]")
+print(odwrotnaMetodaPotegowa(A, sigma=w[0]))
+print("Dla sigma = w[1]")
+print(odwrotnaMetodaPotegowa(A, sigma=w[1]))
+print("Dla sigma = w[2]")
+print(odwrotnaMetodaPotegowa(A, sigma=w[2]))
 
+
+# Jak wyraźnie widać, wyniki te pokrywają się z bibliotecznymi (kolumny / wiersze)
 
 
 
