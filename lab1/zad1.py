81fc107f49d40c0a1154390c43ef87dbdcd48b39
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-05-13 00:49:33 +0200

    Added zad1 file

---
+++ lab1/zad1.py
@@ -0,0 +1,19 @@
+# Sumowanie liczb pojedynczej precyzji
+
+# 1.    Napisz program, który oblicza sumę N liczb pojedynczej precyzji przechowywanych w tablicy
+#       o N = 10^7  elementach. Tablica wypełniona jest tą samą wartością v z przedziału [0.1, 0.9] np. v = 0.53125.
+
+# 2.    Wyznacz bezwzględny i względny błąd obliczeń. Dlaczego błąd względny jest tak duży?
+
+# 3.    W jaki sposób rośnie błąd względny w trakcie sumowania?
+#       Przedstaw wykres (raportuj wartość błędu co 25000 kroków) i dokonaj jego interpretacji.
+
+# 4.    Zaimplementuj rekurencyjny algorytm sumowania, działający jak na rysunku poniżej.
+#       (niczym drzewo binarne)
+
+# 5.    Wyznacz bezwzględny i względny błąd obliczeń. Dlaczego błąd względny znacznie zmalał?
+
+# 6.    Porównaj czas działania obu algorytmów dla tych samych danych wejściowych.
+
+# 7.    Przedstaw przykładowe dane wejściowe, dla których algorytm sumowania rekurencyjnego zwraca niezerowy błąd.
+
