6d642be2d2935413586b3ea43625ae6d76339b2b
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-05-13 03:03:04 +0200

    zad2 init

---
+++ lab1/zad2.py
@@ -0,0 +1,9 @@
+# Algorytm Kahana
+
+# Zaimplementuj algorytm sumowania Kahana.
+
+# 1.    Wyznacz bezwzględny i względny błąd obliczeń dla tych samych danych wejściowych jak w przypadku testów z Zadania 1.
+
+# 2.    Wyjaśnij dlaczego w algorytm Kahana ma znacznie lepsze własności numeryczne? Do czego służy zmienna err?
+
+# 3.    Porównaj czasy działania algorytmu Kahana oraz algorytmu sumowania rekurencyjnego dla tych samych danych wejściowych.
